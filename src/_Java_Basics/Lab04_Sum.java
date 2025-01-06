e510a21118919b3b4a13971bdb13f5666f557cf9
Tushar <tushardhavale2@gmail.com>
2025-01-06 14:48:51 +0530

    Commit

---
+++ src/_Java_Basics/Lab04_Sum.java
@@ -0,0 +1,11 @@
+package java_basics;
+
+public class Lab04_Sum {
+    public static void main(String[] args){
+        int a=5;
+        int b=8;
+        int sum= a+b;
+        System.out.println("sum of a and b" +sum);
+
+    }
+}
