a6a0a0bcb993d5e401114455fc035ca6ef1fce88
Tushar <tushardhavale2@gmail.com>
2025-01-12 09:35:28 +0530

    Commit

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,9 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int number = 9;
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d * %d = %d%n", number, i, number * i);
+        }
+    }
+}
+
