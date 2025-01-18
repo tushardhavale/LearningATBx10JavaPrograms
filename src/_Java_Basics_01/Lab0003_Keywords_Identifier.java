84c40276eff8989cfa8c62cc1b251bda5409c0cc
Tushar <tushardhavale2@gmail.com>
2025-01-18 19:14:09 +0530

    Commit.

---
+++ src/_Java_Basics_01/Lab0003_Keywords_Identifier.java
@@ -0,0 +1,9 @@
+package _Java_Basics_01;
+
+public class Lab0003_Keywords_Identifier
+{
+    public static void main(String[] args)
+    {
+    System.out.println("Hello");
+    }
+}
---
+++ src/_Java_Basics_01/Lab002.java
@@ -0,0 +1,10 @@
+package _Java_Basics_01;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("Tushar");
+    }
+}
+
+
+
