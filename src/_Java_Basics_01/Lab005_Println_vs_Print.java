b9b8a06e3bd49d60b23680e9dddcbc9d50aa5812
Tushar <tushardhavale2@gmail.com>
2025-01-18 20:05:21 +0530

    Commit.

---
+++ src/_Java_Basics_01/Lab005_Println_vs_Print.java
@@ -0,0 +1,13 @@
+package _Java_Basics_01;
+
+public class Lab005_Println_vs_Print
+{
+    public static void main(String[] args) {
+
+//        System.out.println("Hello");
+//        System.out.println("Hi");
+
+        System.out.print("Hello");
+        System.out.print("Hi");
+    }
+}
