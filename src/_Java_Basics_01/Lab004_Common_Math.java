b857b4dca8fa2a5e9ebb9f8a4c04fd5969eab5cd
Tushar <tushardhavale2@gmail.com>
2025-01-18 19:22:51 +0530

    Commit.

---
+++ src/_Java_Basics_01/Lab004_Common_Math.java
@@ -0,0 +1,10 @@
+package _Java_Basics_01;
+
+public class Lab004_Common_Math {
+        public static void main(String[] args) {
+            System.out.println(3+4);
+            System.out.println(5-2);
+            System.out.println(4*5);
+            System.out.println(6/5);
+          }
+}
