5a9a31933a60f4e58f52cc8da2bec5c5186ce8b4
Tushar <tushardhavale2@gmail.com>
2025-01-18 20:17:36 +0530

    Commit.

---
+++ src/_Java_Basics_01/Lab006_Variables.java
@@ -0,0 +1,12 @@
+package _Java_Basics_01;
+
+public class Lab006_Variables
+{
+    public static void main(String[] args)
+    {
+        int age= 10;
+        //data type variable name = variable name
+        age = 35;
+        System.out.println(age);
+    }
+}
