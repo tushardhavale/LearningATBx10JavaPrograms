58c1e08aa81af917c3e7d5f9dd2e02e4360faf36
Tushar <tushardhavale2@gmail.com>
2025-01-20 20:33:35 +0530

    Commit.

---
+++ src/_Java_Basics_01/Lab011_LargestNumber.java
@@ -0,0 +1,13 @@
+package _Java_Basics_01;
+
+public class Lab011_LargestNumber
+{
+    public static void main(String[] args)
+    {
+        int a=15;
+        int b=52;
+        int c=20;
+        int LargestNumber= (a>=b) ? ((a>=c) ? a : c) : ((b>=c) ? b:c);
+        System.out.println(LargestNumber);
+}
+}
