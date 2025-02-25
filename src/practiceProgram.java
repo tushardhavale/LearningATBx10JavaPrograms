3432a70ecbf8f6f217eb77d06c80257617dc650a
Tushar <tushardhavale2@gmail.com>
2025-02-25 16:07:59 +0530

    Commit.

---
+++ src/practiceProgram.java
@@ -0,0 +1,14 @@
+public class practiceProgram {
+    public static void main(String[] args) {
+        int a=50;
+        int b=10;
+        int sum= a+b;
+        int substraction=a-b;
+        int mul=a*b;
+        System.out.println("sum: " +sum);
+        System.out.println("-----------------");
+        System.out.println("sub: " +substraction);
+        System.out.println("-----------------");
+        System.out.println("mul: " +mul);
+    }
+}
