f648ff86287a4c09921155dd7b52fb51900aac87
Tushar <tushardhavale2@gmail.com>
2025-04-14 12:10:48 +0530

    Commit.

---
+++ src/_Java_Basics_03/OddEven.java
@@ -0,0 +1,17 @@
+package _Java_Basics_03;
+
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any number");
+        int number = scanner.nextInt();
+        if (number%2==0) {
+            System.out.println(number + "is even");
+        }else
+        {
+            System.out.println(number+ "is odd");
+        }
+    }
+}
