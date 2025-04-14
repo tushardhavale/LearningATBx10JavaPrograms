3ecda7f3328fea0db75e9a3c2dad31d9dc15b4c6
Tushar <tushardhavale2@gmail.com>
2025-04-14 12:34:05 +0530

    Commit.

---
+++ src/_Java_Basics_03/PrimeNumber.java
@@ -0,0 +1,32 @@
+package _Java_Basics_03;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number");
+        } else {
+            System.out.println(number + " is not a prime number");
+        }
+        scanner.close();
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
